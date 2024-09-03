cce40df7ca5c33967fe4016cae18ce406497b2d4
DunikAndriyani <147916679+DunikAndriyani@users.noreply.github.com>
2024-09-03 14:58:09 +0700

    Add files via upload
    Tugas Praktikum 1

---
+++ mahasiswa/src/main/java/DemoNilai.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author MRvL
+ */
+public class DemoNilai {
+    public static void main(String[] args) {
+        Nilai nilaiMahasiswa = new Nilai("xxxxxxxx", "xxxxxxxx", 00000000, 00000000, 00000000, 0000000);
+
+        nilaiMahasiswa.CetakNilai();
+    }
+}
---
+++ mahasiswa/src/main/java/com/mycompany/mahasiswa/Mahasiswa.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mahasiswa;
+
+/**
+ *
+ * @author MRvL
+ */
+public class Mahasiswa {
+    public void membaca() {
+        System.out.println("Mahasiswa itu sedang membaca buku di kelas.");
+    }
+    
+    public void nyontek() {
+        System.out.println("Mahasiswa menyontek saat ujian berlangsung.");
+    }
+    
+    public void modifikasi() {
+        System.out.println("Mahasiswa sedang memodifikasi tugas yang diberikan dosen.");
+    }
+}
