081f489d810fe03a5d264fd9db53345196921d9f
DunikAndriyani <147916679+DunikAndriyani@users.noreply.github.com>
2024-09-24 22:44:10 +0700

    Pertemuan 4
    Tugas Praktikum PBO

---
+++ BangunRuang.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BangunRuang;
+
+/**
+ *
+ * @author MRvL
+ */
+public class BangunRuang {
+    public double getVolume(){
+        return 0;
+    }
+    
+    public double getLuasPermukaan(){
+        return 0;
+    }
+}
